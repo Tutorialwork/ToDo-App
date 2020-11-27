c1be42c2166a3fbd7edbdd8b490fe4b7565b766d
Manuel <tutorialworktv@gmail.com>
2020-11-27 21:16:08 +0100

    add logic again ;)

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:todo_app/addtodo.dart';
-import 'package:todo_app/checkbox.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:todo_app/navbar.dart';
 import 'package:todo_app/todo.dart';
 
 void main(){
-  runApp(HomePage());
+  runApp(MaterialApp(home: HomePage(),));
 }
 
 class HomePage extends StatefulWidget {
@@ -14,25 +15,119 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  Map<String, dynamic> todo = {};
+  TextEditingController _controller = new TextEditingController();
+  
+  @override
+  void initState() {
+    _loadData();
+  }
+  
+  _loadData() async {
+    SharedPreferences storage = await SharedPreferences.getInstance();
+
+    if (storage.getString('todo') != null) {
+      todo = jsonDecode(storage.getString('todo'));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: SafeArea(
         child: Scaffold(
-          bottomNavigationBar: AddTodo(),
+          bottomNavigationBar: InkWell(
+            onTap: () {
+              showDialog(
+                context: context,
+                builder: (_) {
+                  return AlertDialog(
+                    content: TextFormField(
+                      controller: _controller,
+                      decoration: InputDecoration(
+                        hintText: 'Todo'
+                      ),
+                    ),
+                    actions: [
+                      FlatButton(
+                        child: Text('Speichern'),
+                        onPressed: () {
+                          Navigator.pop(context);
+                          _addTodo();
+                        },
+                      )
+                    ],
+                  );
+                }
+              );
+            },
+            child: Padding(
+              padding: EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.green
+                    ),
+                    child: Icon(Icons.add, color: Colors.white,),
+                  )
+                ],
+              ),
+            ),
+          ),
           body: Column(
             children: [
               Navbar(),
               SizedBox(height: 30,),
-              TodoItem(name: "Hallo Welt", isActive: false),
-              SizedBox(height: 10,),
-              TodoItem(name: "Hallo Welt hashddddddddddddddddddddddddddddddds", isActive: true),
-              SizedBox(height: 10,),
-              TodoItem(name: "Hallo", isActive: true),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: todo.length,
+                  itemBuilder: (context, i) {
+                    return Dismissible(
+                      key: Key('item'+i.toString()),
+                      onDismissed: (direction) {
+                        todo.remove(todo.keys.elementAt(i));
+                        _save();
+                      },
+                      child: InkWell(
+                        onTap: () {
+                          setState(() {
+                            todo[todo.keys.elementAt(i)] = !todo[todo.keys.elementAt(i)];
+                          });
+                          _save();
+                        },
+                        child: TodoItem(name: todo.keys.elementAt(i), isActive: todo.values.elementAt(i)),
+                      ),
+                    );
+                  },
+                ),
+              )
             ],
           )
         ),
       ),
     );
+  }
+
+  _addTodo() async {
+    SharedPreferences storage = await SharedPreferences.getInstance();
+
+    if (_controller.text.length > 0) {
+      setState(() {
+        todo.putIfAbsent(_controller.text, () => false);
+        storage.setString('todo', jsonEncode(todo));
+      });
+    }
+  }
+
+  _save() async {
+    SharedPreferences storage = await SharedPreferences.getInstance();
+
+    storage.setString('todo', jsonEncode(todo));
   }
 }
